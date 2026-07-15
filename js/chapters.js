aca6bbb61113c94c7378f104103f4d1dec29bca7
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-16 00:22:31 +0300

    Implement chapters menu in showChapters function

--- js/chapters.js
+++ js/chapters.js
@@ -1,1 +1,74 @@
+// =========================
+// CHAPTERS MENU
+// =========================
 
+
+function showChapters(){
+
+
+    app.innerHTML = `
+
+
+    <div class="menu-screen">
+
+
+        <h1 class="game-logo">
+            РОЗДІЛИ
+        </h1>
+
+
+
+        <div class="menu-buttons">
+
+
+
+            <button onclick="startScene('scene1')">
+
+                🌙 Пролог
+
+            </button>
+
+
+
+            <button disabled>
+
+                🔒 Глава 1
+
+            </button>
+
+
+
+            <button disabled>
+
+                🔒 Глава 2
+
+            </button>
+
+
+
+            <button disabled>
+
+                🔒 Глава 3
+
+            </button>
+
+
+
+            <button onclick="showMenu()">
+
+                ← Назад
+
+            </button>
+
+
+
+        </div>
+
+
+    </div>
+
+
+    `;
+
+
+}
